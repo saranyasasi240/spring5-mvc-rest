2822026a2834fe53c1e7dcd9ba8bfe9b35ab646d
Prasad <saranyasasi240@gmail.com>
2024-11-04 15:59:23 +0530

    MapStruct | added category service

---
+++ src/main/java/guru/springfamework/service/CategoryServiceImpl.java
@@ -0,0 +1,31 @@
+package guru.springfamework.service;
+
+import guru.springfamework.api.v1.mapper.CategoryMapper;
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.repositories.CategoryRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, CategoryMapper categoryMapper) {
+        this.categoryRepository = categoryRepository;
+        this.categoryMapper = categoryMapper;
+    }
+
+    @Override
+    public List<CategoryDTO> getAllCategories() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(categoryMapper::categoryToCategoryDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CategoryDTO getCategoryByName(String name) {
+        return categoryMapper.categoryToCategoryDTO(categoryRepository.findByName(name));
+    }
+}
