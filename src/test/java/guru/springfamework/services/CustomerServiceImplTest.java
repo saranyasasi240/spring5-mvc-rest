7967d72c251cc58457f29f13df8e40d9c1cc24f1
Prasad <saranyasasi240@gmail.com>
2024-11-04 21:48:50 +0530

    Implemented deletion of a customer

--- src/test/java/guru/springfamework/services/CustomerServiceImplTest.java
+++ src/test/java/guru/springfamework/services/CustomerServiceImplTest.java
@@ -17,7 +17,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class CustomerServiceImplTest {
     @Mock
@@ -115,5 +115,15 @@ public class CustomerServiceImplTest {
         //then
         assertEquals(customerDTO.getFirstname(), savedDto.getFirstname());
         assertEquals("/api/v1/customer/1", savedDto.getCustomerUrl());
+    }
+
+    @Test
+    public void deleteCustomerById() throws Exception {
+
+        Long id = 1L;
+
+        customerService.deleteCustomerById(id);
+
+        verify(customerRepository, times(1)).deleteById(anyLong());
     }
 }
