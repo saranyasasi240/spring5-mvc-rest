32d9c1ffe8435d411ceb075d38bba225c3cc64cc
Prasad <saranyasasi240@gmail.com>
2024-11-04 14:09:08 +0530

    Upgraded to JDK 17 and JUnit 5

--- src/main/java/guru/springfamework/domain/Category.java
+++ src/main/java/guru/springfamework/domain/Category.java
@@ -2,10 +2,10 @@ package guru.springfamework.domain;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 /**
  * Created by jt on 9/24/17.
